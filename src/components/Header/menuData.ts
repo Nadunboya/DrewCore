dede887d727350ed95d21dfe60d73b9609d0df60
Nadunboya <nadunboyagodacrazy@gmail.com>
2026-06-16 13:06:37 +0530

    first commit

---
+++ src/components/Header/menuData.ts
@@ -0,0 +1,22 @@
+import { Menu } from "@/types/Menu";
+
+export const menuData: Menu[] = [
+  {
+    id: 1,
+    title: "Popular",
+    newTab: false,
+    path: "/",
+  },
+  {
+    id: 2,
+    title: "Shop",
+    newTab: false,
+    path: "/shop-with-sidebar",
+  },
+  {
+    id: 3,
+    title: "Contact",
+    newTab: false,
+    path: "/contact",
+  },
+];
